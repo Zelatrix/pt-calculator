e0910bde6a503680de8b242613a51c6e6d889927
Callum Stewart <4onthefly@gmail.com>
2021-01-30 17:40:44 +0000

    Merged repositories

--- pt-calculator-master/pythagoras.erl
+++
@@ -1,8 +0,0 @@
--module(pythagoras).
--export([hypotenuse/2, three_py/2]).
-
-% Computing the length of the hypotenuse of a right-angled triangle. 
-hypotenuse(Adjacent,Opposite) -> math: sqrt(math: pow(Adjacent,2) + math: pow(Opposite,2)).
-
-% m and n are positive coprime integers such that m is greater than n. 
-three_py(M,N) -> math: pow((math: pow(M,2)- math: pow(N,2)),2) + math: pow((2*M*N),2).
--- pythagoras.erl
+++ pythagoras.erl
@@ -1,4 +1,8 @@
 -module(pythagoras).
--export([hypotenuse/2]).
+-export([hypotenuse/2, three_py/2]).
 
+% Computing the length of the hypotenuse of a right-angled triangle. 
 hypotenuse(Adjacent,Opposite) -> math: sqrt(math: pow(Adjacent,2) + math: pow(Opposite,2)).
+
+% m and n are positive coprime integers such that m is greater than n. 
+three_py(M,N) -> math: pow((math: pow(M,2)- math: pow(N,2)),2) + math: pow((2*M*N),2).
